ce15879473ca7266c1a49f6e662ccf37b137232f
DunkJFunk <131308281+DunkJFunk@users.noreply.github.com>
2024-10-21 16:54:12 -0400

    Beta Build v4.0

--- src/components/BeforeAfter.jsx
+++ src/components/BeforeAfter.jsx
@@ -36,7 +36,7 @@ const BeforeAfter = () => {
             {/* <p className="text-center text-xl text-white font-playfair mt-4">Chris Craft XX</p> */}
           </div>
         </div>
-        <button className="text-4xl font-bebasneue pb-10 text-owhite">See more of our work</button>
+        <button className="text-4xl font-bebasneue pb-10 text-owhite" onClick={() => {window.location.href = 'https://www.youtube.com/@classicwoodies'}}>Click Here to see these boats IN ACTION</button>
       </div>
     </div>
   );
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -32,13 +32,13 @@ const LandingPage = () => {
             </div>
             <div className="absolute left-60 top-1/4 font-montserrat w-1/2 text-slate-900 flex-row text-left max-md:left-10 max-md:top-48 max-sm:top-28 max-md:w-full max-lg:relative">
                 <h1 className="text-5xl font-cinzel font-bold w-3/4 max-sm:hidden">RESTORATION DONE THE RIGHT WAY</h1>
-                <div className="text-4xl font-bold mt-4 w-4/5 max-sm:block max-sm:text-4xl">
+                <div className="text-3xl font-bold mt-4 w-4/5 max-sm:block max-sm:text-4xl">
                     <p>- More than 250 restorations</p>
                     <p>- Twenty+ years experience</p>
                     <p>- 5200 Bottoms</p>
                     <p>- Mirror Finishes</p>
                 </div>
-                <button onClick={handleSeeMoreTwo} className="py-4 px-4 bg-slate-900 rounded text-white mt-10 text-xl max-sm:px-8">See More...</button>
+                <button onClick={handleSeeMoreTwo} className="py-3 px-4 bg-slate-900 rounded-lg text-white mt-10 text-xl max-sm:px-8">See More...</button>
                 <button onClick={handleContact} className="py-4 px-4 bg-slate-900 rounded text-white mt-10 text-xl max-sm:px-8 hidden max-sm:block">Contact Us</button>
             </div>
         </div>
